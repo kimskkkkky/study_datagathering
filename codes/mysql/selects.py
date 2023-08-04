ef4c3443dca101f9779dbacf022f9efd1c9d750b
kimskkkkky <ses513a@gmail.com>
2023-08-04 17:58:56 +0900

    first commit

---
+++ codes/mysql/selects.py
@@ -0,0 +1,18 @@
+# mysql connect package
+# pip --> conda
+
+# connect Mysql
+# ip, port, username, password, database name
+
+# editor = statement
+
+# make select query
+
+# execute select query
+
+# return resultset
+
+# return resultset and then display
+
+# close
+
